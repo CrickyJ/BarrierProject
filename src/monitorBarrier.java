a5042be47a70337c0ed3084453c4b441cdde1b03
Ricky Torres <ricdot997@gmail.com>
2019-03-11 16:02:47 -0500

    First Commit

---
+++ src/monitorBarrier.java
@@ -0,0 +1,20 @@
+public class monitorBarrier implements Barrier
+{
+	/*
+	 *	Private members and methods here
+	 */
+	public monitorBarrier(int N)
+	{
+		/*
+		 *	Code for your constructor here
+		 */
+		throw new java.lang.UnsupportedOperationException("Monitor not supported yet.");
+	}
+	public void arriveAndWait()
+	{
+		/*
+		 *	Your code here
+		 */
+		throw new java.lang.UnsupportedOperationException("Monitor not supported yet.");
+	}
+}
---
+++ src/semaphoreBarrier.java
@@ -0,0 +1,87 @@
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class semaphoreBarrier implements Barrier
+{
+	//Private members and methods here
+	private AtomicInteger arrived = new AtomicInteger(0); //
+	private AtomicInteger leaving = new AtomicInteger(0);
+	private int max = 0; //maximum threads barrier will count
+
+	public semaphoreBarrier(int N)
+	{
+		int count = 0;
+		max = N;
+		//throw new java.lang.UnsupportedOperationException("Semaphore not supported yet.");
+	}
+	public void arriveAndWait()
+	{
+
+		//		/*****************************START OF LOOP****************************/
+		//		mutex.wait()
+		//		count += 1
+		//		if (count == n) { //If all threads have been counted
+		//			Barrier2.arriveAndWait() //lock second barrier
+		//			Barrier1.signal() //unlock first barrier
+		//		}
+		//		mutex.signal()
+		//
+		//		Barrier1.arriveAndWait()
+		////BARRIER 1 ----------------------------------------
+		//		Barrier1.signal() //unblock other threads
+		//
+		////CRITICAL SECTION
+		//
+		//		mutex.wait()
+		//		count -= 1;
+		//		if (count == 0) { //If all threads have exited critical section
+		//			Barrier1.arriveAndWait() //lock first barrier
+		//			Barrier2.signal() //unlock second barrier
+		//		}
+		//		mutex.signal()
+		//
+		//		Barrier2.arriveAndWait()
+		////BARRIER 2 ---------------------------------
+		//		Barrier2.signal(); //unblock other threads
+		//
+		///******************************END OF LOOP******************************/
+
+		System.out.println(arrived.get() + " = " + max + "?");
+		if (arrived.incrementAndGet() == max) { //If all threads have been counted
+			lock2();//Barrier 2 lock
+			unlock1();//SignalAll -- Barrier 1 unlock
+        }
+        else {
+			while(arrived.get() > 0);//Wait at barrier
+		}
+
+		System.out.println(leaving.get() + " = " + 0 + "?");
+		if(leaving.decrementAndGet() == 0) {
+			unlock2();
+			lock1();
+		}
+		else {
+			while(leaving.get() < max); //Wait
+		}
+
+		//throw new java.lang.UnsupportedOperationException("Semaphore not supported yet.");
+	}
+
+	private void lock1() { //Lock Barrier1
+		arrived.set(0);
+	}
+
+	private void lock2() {
+		arrived.set(0); //Unlock first barrier
+	}
+
+	private void unlock1() {
+		leaving.set(max); //Unlock first barrier
+		System.out.println("ALL ARRIVED: UNLOCK 1");
+	}
+
+	private void unlock2() {
+		leaving.set(0);
+		System.out.println("ALL ARRIVED: UNLOCK 2");
+	}
+}
