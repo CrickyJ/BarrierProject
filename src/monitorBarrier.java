cea623b8cb3410856433c88e8c561d13af039752
Ricky Torres <ricdot997@gmail.com>
2019-03-13 16:58:11 -0500

    arriveAndWait() method defined

--- src/monitorBarrier.java
+++ src/monitorBarrier.java
@@ -4,38 +4,71 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class monitorBarrier implements Barrier
 {
-	/*
-	 *	Private members and methods here
-	 */
 	enum threadState {arriving, leaving}//condition - barrier is reached at start and end of critical section
 
     private AtomicInteger count = new AtomicInteger(0); //AtomicInteger might be needed to count threads
 
    //private int count = 0; //count threads as they reach barrier
 	private int max; //maximum amount of threads barrier will track
-    private threadState[] state;
+    //private threadState[] state;
+	private threadState state;
 
 	public monitorBarrier(int N)
 	{
 	    max = N;
-		state = new threadState[N]; //every thread will have its own state
+	    state = threadState.arriving;
+//		state = new threadState[max]; //every thread will have its own state
+//		for(int i = 0; i < max; i++) { //for each thread
+//			state[i] = threadState.arriving;
+//		}
 		//throw new java.lang.UnsupportedOperationException("Monitor not supported yet.");
 	}
 	public void arriveAndWait()
 	{
-		initialize(count.incrementAndGet());
-		/*
-		 *	Your code here
-		 */
-		//throw new java.lang.UnsupportedOperationException("Monitor not supported yet.");
+		//threadState current = state[count.incrementAndGet()]; //State of thread calling arriveAndWait()
+		switch(state) {
+			case arriving: //First time arriveAndWait() is called
+				if (count.incrementAndGet() == max) {//if all threads have reached critical section
+					this.signalAll(); //Last thread will wake all other threads
+					state = threadState.leaving;
+				}
+				else { //if some threads have not reached barrier
+					threadWait();
+				}
+				return; //All threads have been signaled, enter critical section
+
+			case leaving: //Second time arriveAndWait() is called
+				if(count.decrementAndGet() == 0) { //All threads have left critical section
+					this.signalAll(); //Wake all threads
+				}
+				else {
+					threadWait(); //Wait until signal
+				}
+				break;
+			default:
+				System.out.println("ERROR: UNKNOWN THREAD STATE");
+				return;
+			//throw new java.lang.UnsupportedOperationException("Monitor not supported yet.");
+		} //END OF switch(state)
+
+		initialize(); //re-initialize thread for re-use
 	}
 
-	private void signal() { //Used when last thread reaches barrier
-	    this.notifyAll(); //Notify all other threads
+	private void threadWait(){ //Thread will wait
+		try {
+			this.wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void signalAll() { //Used when last thread reaches barrier
+		System.out.println("Signal All");
+		this.notifyAll(); //Notify all other threads
     }
 
-	private void initialize(int n) {
-	    state[n] = threadState.arriving; //all threads start as arriving on first arriveAndWait() call
+	private void initialize() {
+	    count.set(0);
 	}
 
 }
