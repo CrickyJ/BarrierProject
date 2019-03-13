83eb05093807461e4cf42676a2b1f30402f6b5eb
Ricky Torres <ricdot997@gmail.com>
2019-03-13 16:00:31 -0500

    Added AtomicIntegers, these are probably best used for count

--- src/monitorBarrier.java
+++ src/monitorBarrier.java
@@ -1,26 +1,41 @@
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
 public class monitorBarrier implements Barrier
 {
 	/*
 	 *	Private members and methods here
 	 */
-	enum state {arriving, leaving}//condition - barrier is reached at start and end of critical section
+	enum threadState {arriving, leaving}//condition - barrier is reached at start and end of critical section
+
+    private AtomicInteger count = new AtomicInteger(0); //AtomicInteger might be needed to count threads
+
+   //private int count = 0; //count threads as they reach barrier
+	private int max; //maximum amount of threads barrier will track
+    private threadState[] state;
 
 	public monitorBarrier(int N)
 	{
-		state[] s = new state[N]; //every thread will have its own state
+	    max = N;
+		state = new threadState[N]; //every thread will have its own state
 		//throw new java.lang.UnsupportedOperationException("Monitor not supported yet.");
 	}
 	public void arriveAndWait()
 	{
-		initialize();
+		initialize(count.incrementAndGet());
 		/*
 		 *	Your code here
 		 */
 		//throw new java.lang.UnsupportedOperationException("Monitor not supported yet.");
 	}
 
-	private void initialize() {
-		//Initialize Monitor?
+	private void signal() { //Used when last thread reaches barrier
+	    this.notifyAll(); //Notify all other threads
+    }
+
+	private void initialize(int n) {
+	    state[n] = threadState.arriving; //all threads start as arriving on first arriveAndWait() call
 	}
 
 }
