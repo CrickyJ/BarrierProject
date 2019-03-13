ca30d4339b9968a6e0074577cd27df240900e2c1
Ricky Torres <ricdot997@gmail.com>
2019-03-13 14:22:12 -0500

    Setting up monitor

--- src/monitorBarrier.java
+++ src/monitorBarrier.java
@@ -3,10 +3,11 @@ public class monitorBarrier implements Barrier
 	/*
 	 *	Private members and methods here
 	 */
+	enum state {arriving, leaving}//condition - barrier is reached at start and end of critical section
+
 	public monitorBarrier(int N)
 	{
-		//condition
-		//state
+		state[] s = new state[N]; //every thread will have its own state
 		//throw new java.lang.UnsupportedOperationException("Monitor not supported yet.");
 	}
 	public void arriveAndWait()
