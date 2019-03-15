5c49b1bc930f6b8a5c73090c71bddcab3000eaba
Ricky Torres <ricdot997@gmail.com>
2019-03-15 15:46:42 -0500

    Revert "SemaphoreBarrier code added"
    This reverts commit 88daf8836c8ab7db14cf387d06b1f9998f6c186f.

--- src/semaphoreBarrier.java
+++ src/semaphoreBarrier.java
@@ -1,49 +1,102 @@
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class semaphoreBarrier implements Barrier {
+public class semaphoreBarrier implements Barrier
+{
 	//Private members and methods here
 	private AtomicInteger arrived = new AtomicInteger(0); //
-	//private AtomicInteger leaving = new AtomicInteger(0);
-	//private int count = 0; // use this instead of AtomicInts
+	private AtomicInteger leaving = new AtomicInteger(0);
 	private int max = 0; //maximum threads barrier will count
-	private boolean isReturning = false;
-	Semaphore mutex = new Semaphore(0), barrier = new Semaphore(1);
+	private boolean firstStart = true;
 
-	public semaphoreBarrier(int N) {
+	public semaphoreBarrier(int N)
+	{
 		max = N;
 		//throw new java.lang.UnsupportedOperationException("Semaphore not supported yet.");
 	}
+	public void arriveAndWait()
+	{
 
-	public void arriveAndWait() {
+		//		/****************************START OF LOOP****************************/
+		//		mutex.wait()
+		//		count += 1
+		//		if (count == n) { //If all threads have been counted
+		//			Barrier2.arriveAndWait() //lock second barrier
+		//			Barrier1.signal() //unlock first barrier
+		//		}
+		//		mutex.signal()
+		//
+		//		Barrier1.arriveAndWait()
+		////BARRIER 1 ----------------------------------------
+		//		Barrier1.signal() //unblock other threads
+		//
+		////CRITICAL SECTION
+		//
+		//		mutex.wait()
+		//		count -= 1;
+		//		if (count == 0) { //If all threads have exited critical section
+		//			Barrier1.arriveAndWait() //lock first barrier
+		//			Barrier2.signal() //unlock second barrier
+		//		}
+		//		mutex.signal()
+		//
+		//		Barrier2.arriveAndWait()
+		////BARRIER 2 ---------------------------------
+		//		Barrier2.signal(); //unblock other threads
+		//
+		///******************************END OF LOOP******************************/
 
-		System.out.println(isReturning);
-		System.out.println(max);
-		if (isReturning) barrier.release();
-		try {
-			//arrived.getAndIncrement();
-			// TODO set acquire limit to max then change boolean to true and release max
-            /*if(mutex.getQueueLength() == max -1) {
-
-                mutex.release(max);
-            }*/
-			mutex.release();
-			mutex.acquire(max);
-			if (!isReturning) {
-				isReturning = true;
-				mutex.release(max * max - max);
-				System.out.println("One has made it through!");
-			} else if (mutex.getQueueLength() == 0) isReturning = false;
-		} catch (Exception e) {
-			e.printStackTrace();
+		if(firstStart) {
+			arrived.incrementAndGet();
+			//System.out.println("Arrived: " + arrived.get() + " = " + max + "?");
+			if (arrived.get() == max) { //If all threads have been counted
+				lock2();//Barrier 2 lock
+				unlock1();//SignalAll -- Barrier 1 unlock
+				firstStart = false;
+			}
+			else {
+				while(arrived.get() > 0)
+					//System.out.println("Still " + arrived.get());
+					;//Wait at barrier until signaled
+			}
 		}
-		try {
-			System.out.println("I got here!");
-			System.out.println(mutex.availablePermits());
-			barrier.acquire();
 
-		} catch (Exception e) {
-			e.printStackTrace();
+		else {
+			leaving.decrementAndGet();
+			//System.out.println("Leaving:" + leaving.get() + " = " + 0 + "?");
+			if(leaving.get() == 0) {
+			    lock1();
+				unlock2();
+				firstStart = true;
+			}
+			else {
+				while(leaving.get() < max)
+					; //Wait
+			}
 		}
+		if(firstStart) {
+			System.out.println("EXIT BARRIER");
+		}
+		//throw new java.lang.UnsupportedOperationException("Semaphore not supported yet.");
+	}
+
+	private void lock1() { //Lock Barrier1, all threads left
+	    arrived.set(0);
+	    System.out.println("LOCK 1");
+	}
+
+	private void lock2() { //Lock Barrier2, all threads arrived
+		leaving.set(max);
+		System.out.println("LOCK 2");
+	}
+
+	private void unlock1() { //Unlock if all threads arrive
+        arrived.set(0); //Unlock first barrier
+		System.out.println("ALL ARRIVED: UNLOCK 1");
+	}
+
+	private void unlock2() { //Unlock if all threads leave
+		leaving.set(max);
+		System.out.println("ALL ARRIVED: UNLOCK 2");
 	}
 }
